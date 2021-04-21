612ff6b1b3ff65911162032d90cba4a7866e4aa3
monchavin <monchavin.pi@odds.team>
2021-04-22 00:24:02 +0700

    create web search ontology from cloud firestore

---
+++ lib/screens/aboutScreen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AboutScreen extends StatefulWidget {
+  @override
+  _AboutScreenState createState() => _AboutScreenState();
+}
+
+class _AboutScreenState extends State<AboutScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('About'),
+        backgroundColor: Colors.black,
+      ),
+      body: Stack(children: [
+        Image.asset(
+          '/bg.jpeg',
+          fit: BoxFit.fill,
+          height: Get.height,
+          width: Get.width,
+        ),
+        Center(
+          child: Container(
+            padding: EdgeInsets.all(32),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 4,
+                  ),
+                ]),
+            child: Text(
+              ' การพัฒนาระบบให้คำแนะนำการซ่อมบำรุงอุปกรณ์คอมพิวเตอร์ตั้งโต๊ะ (PC) โดยใช้ออนโทโลยี \n The system development provides advice on computer (PC) equipment maintenance using ontology \n \n นาย อภิรักษ์ ชินจักร \n รหัสนักศึกษา 6011500680 \n คณะ วิทยาศาสตร์ สาขา วิทยาการคอมพิวเตอร์ \n มหาวิทยาลัยราชภัฏจันทรเกษม \n อาจารย์ที่ปรึกษา อาจารย์ ชัยศิริ สนิทพลกลาง',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 24),
+            ),
+            // color: Colors.red,
+          ),
+        ),
+      ]),
+    );
+  }
+}
---
+++ lib/screens/detail.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:get/get.dart';
+
+class Detail extends StatelessWidget {
+  final data;
+  Detail({Key key, @required this.data}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              appBar(context),
+              Container(
+                padding: EdgeInsets.only(bottom: 32),
+                width: Get.width * 0.5,
+                child: Column(
+                  children: [
+                    data['description'] != ""
+                        ? _buildLayoutDescription(data['description'], 'สาเหตุ')
+                        : Container(),
+                    SizedBox(height: 16),
+                    data['method'] != ""
+                        ? _buildLayoutDescription(data['method'], 'วิธีแก้ไข')
+                        : _buildLayoutDescription('ไม่มีคำอธิบาย', '')
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget appBar(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: Icon(Icons.arrow_back),
+            ),
+          ),
+          Container(
+            child: Column(
+              children: [
+                Text(
+                  data['name'],
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF2F2F3E)),
+                ),
+              ],
+            ),
+          ),
+          Container(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLayoutDescription(String value, String title) {
+    var resultValue = value;
+    return Column(
+      // mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "$title",
+          style: TextStyle(
+              height: 1.5,
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.bold),
+        ),
+        Container(
+          width: Get.width * 0.4,
+          child: Text(
+            "$resultValue",
+            style: TextStyle(
+              height: 1.5,
+              color: Color(0xFF6F8398),
+              fontSize: 16,
+            ),
+            maxLines: null,
+            // softWrap: false,
+          ),
+        ),
+      ],
+    );
+  }
+}
